4a680226079586f9ff727a5dee84c47083990c5f
Erika <erika.yyma@gmail.com>
2019-12-09 09:38:16 +0000

    Created Topic model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   validates :name, presence: true,  length: { maximum: 15 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  valifates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   validates :password, length: { in: 8..32 },format: { with: /\A[A-Za-z]\w*\z/ }
   has_secure_password
+  has_many :topics
 end
